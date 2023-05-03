686cc31037db75eebd410c4d01e06175f3dc5b03
xtLUCIFER <umr0042@gmail.com>
2023-05-03 09:41:14 +0500

    new changes for deployment to vercel

--- app/views.py
+++ app/views.py
@@ -3,12 +3,7 @@ from django.core.cache import cache
 from .models import Info, Projects
 
 def index(request):
-    # Try to get the cached result for this view
-    cached_result = cache.get('index_view')
-    if cached_result is not None:
-        # If a cached result exists, return it
-        return cached_result
-        
+   
     
     # If no cached result exists, get the data and render the template
     data = Info.objects.all()
@@ -17,6 +12,4 @@ def index(request):
         'data': data,
         'projects': all_projects
     }
-    # Cache the result for 1 week
-    cache.set('index_view', render(request, 'index.html', context), 604800)
     return render(request, 'index.html', context)
