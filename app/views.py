d230bfc370274501e6a11c30cbc449de96662233
xtLUCIFER <umr0042@gmail.com>
2023-05-03 14:56:22 +0500

    app restored

--- app/views.py
+++ app/views.py
@@ -3,7 +3,12 @@ from django.core.cache import cache
 from .models import Info, Projects
 
 def index(request):
-   
+    # Try to get the cached result for this view
+    cached_result = cache.get('index_view')
+    if cached_result is not None:
+        # If a cached result exists, return it
+        return cached_result
+        
     
     # If no cached result exists, get the data and render the template
     data = Info.objects.all()
@@ -12,4 +17,6 @@ def index(request):
         'data': data,
         'projects': all_projects
     }
+    # Cache the result for 1 week
+    cache.set('index_view', render(request, 'index.html', context), 604800)
     return render(request, 'index.html', context)
