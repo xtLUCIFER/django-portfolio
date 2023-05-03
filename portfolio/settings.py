f9e834f4db96ac1f358934c19b75911e476e4fed
xtLUCIFER <umr0042@gmail.com>
2023-05-03 14:48:58 +0500

    database changed to sqlite

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,16 +79,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 
 
 # Password validation
